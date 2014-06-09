c26367f25b7f5d7c1bd9b383b1a8ed9cd91f834a
ronaldo <ronaldo.possan@gmail.com>
2014-06-09 17:39:35 -0300

    Changing method name and adding method as privat of the main class

--- beginner/level4.rb
+++ beginner/level4.rb
@@ -1,16 +1,10 @@
 # Now warrior should continue with the same algorithm but have to pay attention in the archer
 # When warrior take damage have to continue walking to don't take the arrows damage
 class Player
-  # A helper method to check if warrior need or not to rest
-  def need_rest?(warrior)
-    # If warrior is not taking damage and less than 15 need to rest
-    warrior.health < 15 && (warrior.health >= @health)
-  end
-  
   def play_turn(warrior)
     if warrior.feel.empty?
       #Using the method to check
-      if need_rest?(warrior)
+      if should_rest?(warrior)
         warrior.rest!
       else
         warrior.walk!
@@ -19,5 +13,12 @@ class Player
       warrior.attack!
     end
     @health = warrior.health
+  end
+
+  private
+  # A helper method to check if warrior need or not to rest
+  def should_rest?(warrior)
+    # If warrior is not taking damage and less than 15 need to rest
+    warrior.health < 15 && (warrior.health >= @health)
   end
 end
